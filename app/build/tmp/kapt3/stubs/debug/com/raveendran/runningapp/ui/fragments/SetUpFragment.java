700d474cc1e47eeb512fb429fda0084d1396460a
Raveendran N <ravindran1307@gmail.com>
2021-07-14 17:31:26 +0530

    stop run

---
+++ app/build/tmp/kapt3/stubs/debug/com/raveendran/runningapp/ui/fragments/SetUpFragment.java
@@ -0,0 +1,18 @@
+package com.raveendran.runningapp.ui.fragments;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u001a\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u00062\b\u0010\u0007\u001a\u0004\u0018\u00010\bH\u0016\u00a8\u0006\t"}, d2 = {"Lcom/raveendran/runningapp/ui/fragments/SetUpFragment;", "Landroidx/fragment/app/Fragment;", "()V", "onViewCreated", "", "view", "Landroid/view/View;", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"})
+public final class SetUpFragment extends androidx.fragment.app.Fragment {
+    private java.util.HashMap _$_findViewCache;
+    
+    @java.lang.Override()
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    public SetUpFragment() {
+        super();
+    }
+}
